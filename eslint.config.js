c6bebc7aaf3b1b70501f54c186f3f456d7d748e0
RosieOh <dhxogns920@gmail.com>
2026-07-18 14:01:04 +0900

    CHORE : Flat 구성 방식의 ESLint 설정(eslint.config.js)을 추가한다.

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'dist-electron', 'release', 'node_modules', '*.config.js', '*.config.ts'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: { ...globals.node, ...globals.browser },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+)
